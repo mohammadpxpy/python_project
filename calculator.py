5ab93ed20041e304020a7b534b37e847af750fd6
mohammad_px.py <mohammadmhmdy192@gmail.com>
2025-09-26 20:18:40 +0330

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+print("calculator")
+
+def calculator():
+    print("calculator")
+    print("(+ , - , * , / )")
+    
+    num1 = float(input(" ENTER number: "))
+    op = input("<< + ,  _  , * , / >> :")
+    num2 = float(input(" ENTER number: "))
+    
+    if op == "+" :
+        result = num1 + num2
+    elif op == "-" :
+        result = num1 - num2
+    elif op == "*" :
+        result = num1 * num2
+    elif op == "/" :
+        if num2 != 0 :
+            result = num1 / num2
+        else :
+            result = "not true"
+    else:
+        result = "not true"
+        
+    print(f" => {result}")
+
+calculator()
