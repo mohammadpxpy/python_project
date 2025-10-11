182c3d36fe65e2ec0d62cf38116c103988ed72a4
mohammad_px.py <mohammadmhmdy192@gmail.com>
2025-10-11 21:14:07 +0330

    Add files via upload

---
+++ Star_triangle.py
@@ -0,0 +1,14 @@
+import time
+
+space = 12
+star = 1
+
+for i in range(1, 10):
+    print(i,end="\r")
+    time.sleep(1)
+    print(" " * space , end=" ")
+    print(i * " * ")
+        
+    star += 1
+    space -= 1
+        
